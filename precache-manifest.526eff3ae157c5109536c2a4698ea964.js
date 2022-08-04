7907ffc0c84bfccf1d3770d68306c5ac1003e2ea
RiBpy <77004199+RiBpy@users.noreply.github.com>
2022-08-05 01:02:55 +0600

    Updates

---
+++ precache-manifest.526eff3ae157c5109536c2a4698ea964.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "506a17115d7ffd9fbd21bb0b708c1894",
+    "url": "/react_simple_tour_app/index.html"
+  },
+  {
+    "revision": "f18c9487489a7547f928",
+    "url": "/react_simple_tour_app/static/css/main.c3e53c0d.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/react_simple_tour_app/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_simple_tour_app/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f18c9487489a7547f928",
+    "url": "/react_simple_tour_app/static/js/main.9e46a255.chunk.js"
+  },
+  {
+    "revision": "3ca03e3241cdd448b5b8",
+    "url": "/react_simple_tour_app/static/js/runtime-main.1db49cdd.js"
+  }
+]);
